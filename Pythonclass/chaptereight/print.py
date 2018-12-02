83c59c8c094d12d932a63c7a8b179dc0cb05fa0f
Shawn <Shawntarver7@gmail.com>
2018-12-02 02:18:47 +0100

    Python class projects
    Python projects

---
+++ Pythonclass/chaptereight/print.py
@@ -0,0 +1,5 @@
+from printing_functions import print_models, show_completed_models
+
+print_models.printing_functions(unprinted_designs, completed_models)
+show_completed_models.printing_functions(completed_models)
+help(print_models)
