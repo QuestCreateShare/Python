6b75519830dac7cbf8d13a4ccea3837514e8c783
Shawn <Shawntarver7@gmail.com>
2018-12-02 18:15:05 +0100

    getting there

--- Pythonclass/Chapternine/dog.py
+++ Pythonclass/Chapternine/dog.py
@@ -1,18 +1,19 @@
 class Dog():
   """A simple attempt to model a dog."""
 
-def __init__(self, name, age):
+  def __init__(self, name, age):
     """Initialize name and age attributes."""
     self.name = name
     self.age = age
 
-def sit(self):
-    """Simulate a dog sitting in response to a command."""
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
     print(self.name.title() + " is now sitting.")
 
-def roll_over(self):
-    """Simulate rolling over in response to a command."""
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
     print(self.name.title() + " rolled over!")
+
 
 
 my_dog = Dog('Wikkie', 6)
