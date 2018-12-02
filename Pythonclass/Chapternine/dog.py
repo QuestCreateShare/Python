699bcf2ddcd123d8d18ed78175a7c3f3f28b892f
Shawn <Shawntarver7@gmail.com>
2018-12-02 03:56:37 +0100

    Damn python

--- Pythonclass/Chapternine/dog.py
+++ Pythonclass/Chapternine/dog.py
@@ -1,14 +1,15 @@
-class  Dog():
-    """ A simple attempt to model a dog."""
-    def __init__(self, name , age):
-        """Initialize name and age attributes."""
-        self.name = name
-        self.age = age
+class Dog():
+  """A simple attempt to model a dog."""
 
-    def sit(self):
-        """ Simulate a dog sitting in response to a command."""
-        print(self.name.title() + " is now  sitting.")
+def __init__(self, name, age):
+    """Initialize name and age attributes."""
+    self.name = name
+    self.age = age
 
-    def roll_over(self):
-        """Simulate rolling over in response to a command."""
-        print(self.name.title() + "rolled over!")
+def sit(self):
+    """Simulate a dog sitting in response to a command."""
+    print(self.name.title() + " is now sitting.")
+
+def roll_over(self):
+    """Simulate rolling over in response to a command."""
+    print(self.name.title() + " rolled over!")
