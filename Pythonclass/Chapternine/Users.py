fd5960f316919b7fd71a139fb9573d762fa715f6
Shawn <Shawntarver7@gmail.com>
2018-12-03 21:14:24 +0100

    making an army

---
+++ Pythonclass/Chapternine/Users.py
@@ -0,0 +1,12 @@
+class User():
+""" Make a user"""
+def __init__(self, first_name, last_name, age, account_id):
+    """ Initialize first and last name attributes"""
+    self.first_name = first_name
+    self.last_name  = last_name
+    self.age = age
+    self.account_id = account_id
+
+def describe_user(self):
+    """Describe user"""
+    
