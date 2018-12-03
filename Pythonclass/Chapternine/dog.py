8e36bdbb26562c33b31c9d5df371606ec2e8cf6b
Shawn <Shawntarver7@gmail.com>
2018-12-03 03:13:43 +0100

    Onto the "that part"

--- Pythonclass/Chapternine/dog.py
+++ Pythonclass/Chapternine/dog.py
@@ -16,10 +16,13 @@ class Dog():
 
 
 
-my_dog = Dog('Wikkie', 6)
-my_dog.sit()
-my_dog.roll_over()
+my_dog = Dog('Willie', 6)
+your_dog = Dog('lucy', 3)
 
 print("My dog's name is " + my_dog.name.title() + ".")
 print("My dog is " + str(my_dog.age) + " years old.")
-my_dog.name
+my_dog.sit()
+
+print("\nyour dog's name is " + your_dog.name.title() + ".")
+print("Your dog is " + str(your_dog.age) + " years old. ")
+your_dog.sit()
