a3084393b5e68a69edc3a925a44eb81348ebb1dd
Shawn <Shawntarver7@gmail.com>
2018-12-05 22:28:29 +0100

    get wrecked

--- Pythonclass/Chapternine/restaurant.py
+++ Pythonclass/Chapternine/restaurant.py
@@ -4,19 +4,19 @@ class Restaurant ():
         """ Initializatize restaurant and cuisine attributes."""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-        self.number_served
+        self.number_served = 0
 
     def  describe_restaurant(self):
         """ Describe the restaurant"""
         print(self.restaurant_name.title() + " serves " + self.cuisine_type.title() + ".")
 
-
     def set_number_served(self):
         """Show customers that have been serverd"""
         print(" The restaurant has served: " + str(self.number_served) + " customers.")
 
-    def increment_number_served(self, number_served):
-        
+    def increment_number_served(self,served):
+        """ Add number of served guest to number served"""
+        self.number_served += served
 
     def  open_restaurant(self):
       """ The Restaurant is open"""
@@ -26,8 +26,12 @@ class Restaurant ():
 restaurant = Restaurant('Hi',' Japanese')
 restaurant.describe_restaurant()
 
-restaurant.number_served = 10
+restaurant.number_served = 0
 restaurant.set_number_served()
+
+restaurant.increment_number_served(20)
+restaurant.set_number_served()
+
 restaurant.open_restaurant()
 
 
