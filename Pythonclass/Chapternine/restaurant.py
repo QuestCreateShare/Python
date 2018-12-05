09f562cb273d5b0b84f0ed86bdcdba36ffe3fb26
Shawn <Shawntarver7@gmail.com>
2018-12-05 21:08:25 +0100

    boom we are rolling

--- Pythonclass/Chapternine/restaurant.py
+++ Pythonclass/Chapternine/restaurant.py
@@ -4,10 +4,16 @@ class Restaurant ():
         """ Initializatize restaurant and cuisine attributes."""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served =
 
     def  describe_restaurant(self):
         """ Describe the restaurant"""
         print(self.restaurant_name.title() + " serves " + self.cuisine_type.title() + ".")
+
+
+    def set_number_served(self):++
+        """Show customers that have been serverd"""
+        print(" The restaurant has served: " + str(self.number_served) + " customers.")
 
     def  open_restaurant(self):
       """ The Restaurant is open"""
@@ -16,7 +22,11 @@ class Restaurant ():
 
 restaurant = Restaurant('Hi',' Japanese')
 restaurant.describe_restaurant()
+
+restaurant.number_served = 10
+restaurant.set_number_served()
 restaurant.open_restaurant()
+
 
 print("\nThe restaurant is called :" +  restaurant.restaurant_name.title())
 print("The restaurant serves :" + restaurant.cuisine_type + " cuisine ")
