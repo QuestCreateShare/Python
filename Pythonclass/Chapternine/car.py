7d50027aad47cc9515f4aff3c44eae5df6f04737
Shawn <Shawntarver7@gmail.com>
2018-12-08 18:16:51 +0100

    making childern LOL

--- Pythonclass/Chapternine/car.py
+++ Pythonclass/Chapternine/car.py
@@ -31,6 +31,16 @@ class Car():
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles"""
+
+    def __init__(self, make, model, year):
+       """Initialize attributes of the parent class."""
+       super().__init__(make,model, year)
+
+my_tesla = ElectricCar('tesla', ' model s', 2016)
+print(my_tesla.get_descriptive_name())
+
 my_used_car = Car('subaru', 'outback', 2013)
 print(my_used_car.get_descriptive_name())
 
