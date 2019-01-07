1978dd7c7a50d15140b0c9b3b860668b95922c4d
Shawn <Shawntarver7@gmail.com>
2019-01-08 00:53:55 +0100

    Pi day!

--- Pythonclass/Chapterten/file_reader.py
+++ Pythonclass/Chapterten/file_reader.py
@@ -1,3 +1,8 @@
- with open('pi_digits.txt') as file_object:
-     contents = file_object.read()
-     print(contents)
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
+     #contents = file_object.read()
+     #print(contents.rstrip())
