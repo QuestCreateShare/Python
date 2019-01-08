f54b3c6f26f95ffaf85999c4dbe4a4662f32e674
Shawn <Shawntarver7@gmail.com>
2019-01-08 12:35:50 +0100

    working on some guest stuff

--- Pythonclass/Chapterten/guest.py
+++ Pythonclass/Chapterten/guest.py
@@ -2,4 +2,4 @@ filename = 'new_guest.txt'
 name = input("Write name")
 
 with open(filename, 'w') as file_object:
-    file_object.write("Their name is: " + name)
+    file_object.write("Their name is: " + name.title())
