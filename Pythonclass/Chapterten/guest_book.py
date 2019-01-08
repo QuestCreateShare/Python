445b3a5d14d00514fb35e2828e3c1b59e84ee273
Shawn <Shawntarver7@gmail.com>
2019-01-08 21:24:05 +0100

    We good

--- Pythonclass/Chapterten/guest_book.py
+++ Pythonclass/Chapterten/guest_book.py
@@ -1,15 +1,25 @@
 filename = 'book.txt'
-prompt= " Please enter your name here: "
+prompt= " \nPlease enter your name here: "
 prompt += "\nEnter 'quit' to after you have entered in your name"
 
 
-mesasge = " "
-while  message != 'quit':
-    message = input(prompt)
-    user = unregistered_users
-
-    with open(filename, 'w') as file_object:
-        file_object.write("Greeetings welcome to the otherside." + user.title() + "\n")
+while True:
+    user = input(prompt)
 
     with open(filename, 'a') as file_object:
-        file_object.write("Thank you for your visit. It's nice to see you again Mr." + user.title() + "\n")
+        if user =='quit':
+            break
+        else:
+            file_object.write("Greeetings welcome to the otherside." + user.title() + "\n")
+            file_object.write("Thank you for your visit. It's nice to see you again Mr." + user.title() + "\n")
+
+    with open(filename) as file_object:
+         lines = file_object.readlines()
+
+         for line in lines:
+             print(line.strip())
+
+#TestFunction 
+def reset():
+    with open(filename,'w') as file_object:
+        file_object.write('')
