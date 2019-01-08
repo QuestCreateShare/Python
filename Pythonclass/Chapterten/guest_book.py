9c5993f10963f98d8b09a7508c8016ec143dc54a
Shawn <Shawntarver7@gmail.com>
2019-01-08 12:54:57 +0100

    While looping

--- Pythonclass/Chapterten/guest_book.py
+++ Pythonclass/Chapterten/guest_book.py
@@ -1,14 +1,8 @@
 filename = 'book.txt'
-user = input()
-
-while user == True:
+while user:
     user = input(" Please enter your name here: ")
+    with open(filename, 'w') as file_object:
+        file_object.write("Greeetings welcome to the otherside." + user.title() + "\n")
 
-
-with open(filename, 'w') as file_object:
-    file_object.write("Greeetings welcome to the otherside." + user.title())
-
-with open(filename, 'a') as file_object:
-    file_object.write("Thank you for your visit. It's nice to see you again Mr." + user.title())
-
-return user
+    with open(filename, 'a') as file_object:
+        file_object.write("Thank you for your visit. It's nice to see you again Mr." + user.title() + "\n")
