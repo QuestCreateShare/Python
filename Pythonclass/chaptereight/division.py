b68c4a5e88b01bdbdcf00f10bd41b28f928f3a7e
Shawn <Shawntarver7@gmail.com>
2019-01-08 21:45:34 +0100

    You're the exception

--- Pythonclass/chaptereight/division.py
+++ Pythonclass/chaptereight/division.py
@@ -1,4 +1,20 @@
+#Working with try exception
 try:
    print(5/0)
 except ZeroDivisionError:
     print("You can't divide by zero!")
+
+#Using Exceptions to prevent crashes
+
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    answer = int(first_number)/ int(second_number)
+    print(answer)
