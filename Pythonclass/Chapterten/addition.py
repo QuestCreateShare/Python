47e71d4c8a618aedc25d6702dcc648fe04ad9dac
Shawn <Shawntarver7@gmail.com>
2019-01-10 13:00:34 +0100

    caught em

---
+++ Pythonclass/Chapterten/addition.py
@@ -0,0 +1,20 @@
+#Write a program that prevent you from seeing an error.
+#When you try to add a string and a interger
+
+print(" Give me two numbers and I will add them for you.")
+print("Enter 'q' to quit.")
+
+
+while True:
+     first_number = input("\nFirst number: ")
+     if first_number == 'q':
+         break
+
+     second_number = input("\nSecond_number: ")
+     try:
+        answer = int(first_number) + int(second_number)
+     except ValueError:
+        print( "you can't add intergers and strings!")
+
+     else:
+        print(answer)
