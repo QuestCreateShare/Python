8a7c49aa003ef1509d3e8cb176eac7f471679f1e
Shawn <Shawntarver7@gmail.com>
2019-01-16 01:20:09 +0100

    test

--- Pythonclass/Chaptereleven/name_function.py
+++ Pythonclass/Chaptereleven/name_function.py
@@ -1,5 +1,4 @@
 def get_formatted_name(first, last):
     """Generatea neatly formatted full name."""
-    full_name = first + '' + last
+    full_name = first + ' ' + last
     return full_name.title()
-    
--- Pythonclass/Chaptereleven/names.py
+++ Pythonclass/Chaptereleven/names.py
@@ -1,4 +1,4 @@
-from name_function import get_formatted_name
+ from name_function import get_formatted_name
 
 print("Enter 'q' at any time to quit.")
 while True:
